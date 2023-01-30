5563fcab864661ed6bb47e5904f518c8bfaec979
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-01-30 10:36:39 +0100

    color-flipper con js

--- color-flipper/main.js
+++ color-flipper/main.js
@@ -1,1 +1,30 @@
-import './style.css'
+import "./style.css";
+
+const COLOR_PALETTE = {
+  "Por defecto": "#FFFFFF",
+  "Raisin Black": "#28262C",
+  "Blue Bell": "#998FC7",
+  "Lavender Blue": "#D4C2FC",
+  Magnolia: "#F9F5FF",
+  "Resolution Blue": "#14248A",
+};
+const select = document.querySelector(".color-picker");
+const h2 = document.querySelector(".h2")
+
+const createOptions = () => {
+  Object.keys(COLOR_PALETTE).forEach((color) => {
+    const option = document.createElement("option");
+    option.innerText = color;
+    select.append(option);
+  });
+};
+const AELselect = () => {
+  select.addEventListener("change", (ev) => {
+    const color = ev.currentTarget.value;
+    document.body.style.backgroundColor = COLOR_PALETTE[color];
+    h2.innerHTML = `${color} | ${COLOR_PALETTE[color]}`
+  });
+};
+
+createOptions();
+AELselect();
