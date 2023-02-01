2d917d598398d5320dd35b0611316f2d14a80bed
Rgrivas9 <rafaelgrcrvs@gmail.com>
2023-02-01 10:43:52 +0100

    ..

--- HUB DE APPS/AppHub/src/components/mainButton.ts
+++ HUB DE APPS/AppHub/src/components/mainButton.ts
@@ -1,9 +1,15 @@
 export const mainButton = (): HTMLButtonElement => {
-    const btn: HTMLButtonElement = document.createElement("button");
-    const h3: HTMLHeadingElement = document.createElement("h3");
-    h3.innerHTML = "Start";
-    btn.appendChild(h3);
-    btn.setAttribute('id','mainbutton')
-    return btn;
-  };
-  
+  const btn: HTMLButtonElement = document.createElement("button");
+  const h3: HTMLHeadingElement = document.createElement("h3");
+  h3.innerHTML = "Start";
+  btn.appendChild(h3);
+  btn.setAttribute("id", "mainbutton");
+  btn.addEventListener("click", () => {
+    const video = document.querySelector<HTMLVideoElement>('#videoFalcon') as HTMLVideoElement
+    video.removeAttribute('class')
+    video.setAttribute("class", "videoFalcon");
+    video.setAttribute('muted','false')
+    video.play()
+  });
+  return btn;
+};
--- HUB DE APPS/AppHub/src/pages/00.main/00main.ts
+++ HUB DE APPS/AppHub/src/pages/00.main/00main.ts
@@ -11,22 +11,56 @@ export const main = (): void => {
   const header = document.querySelector<HTMLElement>("header") as HTMLElement;
   const main = document.querySelector<HTMLElement>("main") as HTMLElement;
   const footer = document.querySelector<HTMLElement>("footer") as HTMLElement;
+  const body = document.querySelector<HTMLBodyElement>(
+    "body"
+  ) as HTMLBodyElement;
   const h1: HTMLHeadingElement = document.createElement("h1");
   const h3: HTMLHeadingElement = document.createElement("h3");
   const div: HTMLDivElement = document.createElement("div");
-  const ul:HTMLUListElement=document.createElement('ul')
+  const ul: HTMLUListElement = document.createElement("ul");
+  const video: HTMLVideoElement = document.createElement("video");
+  const src: HTMLSourceElement = document.createElement("source");
+  src.setAttribute(
+    "src",
+    "https://res.cloudinary.com/di0zpa5yw/video/upload/v1675239845/gamesHub/Millennium_falcon_hyperdrive_effect_tukotn.mp4"
+  );
+  src.setAttribute("type", "video/mp4");
+  video.setAttribute("autoplay", "false");
+  video.setAttribute("muted", "true");
+  video.setAttribute("id", "videoFalcon");
+  video.setAttribute("class", "hidden");
+  video.appendChild(src);
   clean(header);
   clean(main);
   clean(footer);
-  header.appendChild(switchButton("mainSwitchTheme",'https://res.cloudinary.com/di0zpa5yw/image/upload/v1675189215/gamesHub/paint-palette_hgwh5c.png'));
+  header.appendChild(
+    switchButton(
+      "mainSwitchTheme",
+      "https://res.cloudinary.com/di0zpa5yw/image/upload/v1675189215/gamesHub/paint-palette_hgwh5c.png"
+    )
+  );
   h1.innerHTML = "Duel of the games";
   h3.innerHTML = "Introduce tu nombre";
+  body.appendChild(video);
   div.appendChild(h1);
   div.appendChild(h3);
-  div.appendChild(mainInput())
+  div.appendChild(mainInput());
   div.appendChild(mainButton());
   main.appendChild(div);
-  ul.appendChild(mediaAnchor('https://res.cloudinary.com/di0zpa5yw/image/upload/v1675183434/gamesHub/github_cxjker.png','https://github.com/Rgrivas9','Github'))
-  ul.appendChild(mediaAnchor('https://res.cloudinary.com/di0zpa5yw/image/upload/v1675183434/gamesHub/vercel_xqulbz.png','https://vercel.com/rgrivas9','Vercel',0))
-  footer.appendChild(ul)
+  ul.appendChild(
+    mediaAnchor(
+      "https://res.cloudinary.com/di0zpa5yw/image/upload/v1675183434/gamesHub/github_cxjker.png",
+      "https://github.com/Rgrivas9",
+      "Github"
+    )
+  );
+  ul.appendChild(
+    mediaAnchor(
+      "https://res.cloudinary.com/di0zpa5yw/image/upload/v1675183434/gamesHub/vercel_xqulbz.png",
+      "https://vercel.com/rgrivas9",
+      "Vercel",
+      0
+    )
+  );
+  footer.appendChild(ul);
 };
